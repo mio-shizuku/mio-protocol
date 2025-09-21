98e32fed4147c7fe502c9d4338f85d74051c31cd
Yttehs <shetty.yttehs@outlook.com>
2025-09-21 15:10:36 +0800

    feat(payload): impl more conversions

--- src/payload/mod.rs
+++ src/payload/mod.rs
@@ -31,8 +31,26 @@ where
         })
     }
 
+    pub fn from_json_string(json: &str) -> Result<Self, error::PayloadError> {
+        serde_json::from_str(json).map_err(|e| {
+            error::PayloadError::DeserializationError(error::DeserializationError::JsonError(e))
+        })
+    }
+
     pub fn to_json(&self) -> Result<serde_json::Value, error::PayloadError> {
         serde_json::to_value(self).map_err(|e| {
+            error::PayloadError::SerializationError(error::SerializationError::JsonError(e))
+        })
+    }
+
+    pub fn to_json_string(&self) -> Result<String, error::PayloadError> {
+        serde_json::to_string(self).map_err(|e| {
+            error::PayloadError::SerializationError(error::SerializationError::JsonError(e))
+        })
+    }
+
+    pub fn to_json_string_pretty(&self) -> Result<String, error::PayloadError> {
+        serde_json::to_string_pretty(self).map_err(|e| {
             error::PayloadError::SerializationError(error::SerializationError::JsonError(e))
         })
     }
