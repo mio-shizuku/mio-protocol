1944b9d739b8d661782e6ba41049a331dc96e51d
Yttehs <shetty.yttehs@outlook.com>
2025-09-21 16:26:43 +0800

    test: use expect to replace match

--- src/model/auth/pin/test.rs
+++ src/model/auth/pin/test.rs
@@ -4,10 +4,9 @@ fn test_pin_payload() {
 
     let payload = crate::payload::Payload::new(crate::payload::action::PayloadAction::Auth, pin);
 
-    let json = match payload.to_json() {
-        Ok(json_value) => json_value,
-        Err(e) => panic!("Failed to serialize payload to JSON: {:?}", e),
-    };
+    let json = payload
+        .to_json()
+        .expect("Failed to serialize payload to JSON");
 
     let expected_json = serde_json::json!({
         "action": "auth",
