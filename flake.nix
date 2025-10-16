29b7fbf08809c9dfc42f48ddb6a0afdc3c1372f4
Shetty Yttehs <shetty.yttehs@outlook.com>
2025-10-16 23:04:16 +0800

    build(flake): add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dev shell for mio-protocol";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ rustup ];
+      };
+
+      shellHook = ''
+        rustup default stable
+        echo "Nix dev shell"
+      '';
+    };
+}
+
